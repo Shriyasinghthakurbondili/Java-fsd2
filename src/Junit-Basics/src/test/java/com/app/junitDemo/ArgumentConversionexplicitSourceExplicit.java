f9529fdbe05a1d4df5cf0d92b547eb43ff2ebd79
Shriyasinghthakurbondili <144973815+Shriyasinghthakurbondili@users.noreply.github.com>
2023-10-16 22:29:17 +0530

    Add files via upload

---
+++ src/Junit-Basics/src/test/java/com/app/junitDemo/ArgumentConversionexplicitSourceExplicit.java
@@ -0,0 +1,22 @@
+package com.app.junitDemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.AnnotationBasedArgumentConverter;
+import org.junit.jupiter.params.converter.ConvertWith;
+//import org.junit.jupiter.params.converter.TypedArgumentConverter;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class ArgumentConversionexplicitSourceExplicit {
+  
+	@ParameterizedTest
+	@EnumSource(TimeUnit.class)
+	void testCaseWithExplicitArgumentConversion(
+			@ConvertWith(ToStringArgumentConverter.class) String argument)
+	{
+		Assertions.assertNotNull(TimeUnit.valueOf(argument));
+	}
+	
+}
